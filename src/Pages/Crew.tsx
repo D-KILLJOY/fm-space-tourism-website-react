768a6d49c404ee5a997714597412d5ca153ce670
Dev KILLJOY <didiauche@gmail.com>
2025-11-26 07:23:15 +0100

    fixed css

--- src/Pages/Crew.tsx
+++ src/Pages/Crew.tsx
@@ -10,67 +10,58 @@ function Crew() {
     }
 
     return (
-        <div>
-            <main className="crew__main__con">
-                {crewData.map(
-                    (crew) =>
-                        crew.name === crewName && (
-                            <section
-                                className="crew__dets__con"
-                                key={crew.name}
-                            >
-                                <p className="intro__text">
-                                    <span className="index">02</span>Meet your
-                                    crew
-                                </p>
-                                <section className="crew__dets">
-                                    <article className="crew__detail">
-                                        <div className="crew__det">
-                                            <h2 className="crew__position">
-                                                {crew.role}
-                                            </h2>
-                                            <h1 className="crew__name">
-                                                {crew.name}
-                                            </h1>
-                                            <p className="crew__bio">
-                                                {crew.bio}
-                                            </p>
-                                        </div>
-                                        <nav className="crew__nav__con">
-                                            {crewData.map((crewNav) => (
-                                                <div
-                                                    onClick={() =>
-                                                        changeCrew(crewNav.name)
-                                                    }
-                                                    key={crewNav.name}
-                                                    className={`crew__nav__item ${
-                                                        crewNav.name ===
-                                                        crewName
-                                                            ? "crew__nav__item--active"
-                                                            : ""
-                                                    }`}
-                                                ></div>
-                                            ))}
-                                        </nav>
-                                    </article>
-                                    <article className="crew__img__con">
-                                        <picture className="crew__img">
-                                            <source
-                                                srcSet={`src/${crew.images.webp}`}
-                                                type="image/webp"
-                                            />
-                                            <img
-                                                src={`src/${crew.images.png}`}
-                                                alt="Image of ${data.crew[crewIndex].name}"
-                                            />
-                                        </picture>
-                                    </article>
-                                </section>
+        <main className="crew__main__con">
+            {crewData.map(
+                (crew) =>
+                    crew.name === crewName && (
+                        <section className="crew__dets__con" key={crew.name}>
+                            <p className="intro__text">
+                                <span className="index">02</span>Meet your crew
+                            </p>
+                            <section className="crew__dets">
+                                <article className="crew__detail">
+                                    <div className="crew__det">
+                                        <h2 className="crew__position">
+                                            {crew.role}
+                                        </h2>
+                                        <h1 className="crew__name">
+                                            {crew.name}
+                                        </h1>
+                                        <p className="crew__bio">{crew.bio}</p>
+                                    </div>
+                                    <nav className="crew__nav__con">
+                                        {crewData.map((crewNav) => (
+                                            <div
+                                                onClick={() =>
+                                                    changeCrew(crewNav.name)
+                                                }
+                                                key={crewNav.name}
+                                                className={`crew__nav__item ${
+                                                    crewNav.name === crewName
+                                                        ? "crew__nav__item--active"
+                                                        : ""
+                                                }`}
+                                            ></div>
+                                        ))}
+                                    </nav>
+                                </article>
+                                <article className="crew__img__con">
+                                    <picture className="crew__img">
+                                        <source
+                                            srcSet={`src/${crew.images.webp}`}
+                                            type="image/webp"
+                                        />
+                                        <img
+                                            src={`src/${crew.images.png}`}
+                                            alt="Image of ${data.crew[crewIndex].name}"
+                                        />
+                                    </picture>
+                                </article>
                             </section>
-                        )
-                )}
-            </main>
-        </div>
+                        </section>
+                    )
+            )}
+        </main>
     );
 }
 
--- src/Pages/Technology.tsx
+++ src/Pages/Technology.tsx
@@ -8,80 +8,73 @@ function Tecnology() {
         setTechName(tech);
     }
     return (
-        <>
-            <main className="tech__main__con">
-                {technologyData.map(
-                    (technology) =>
-                        techName === technology.name && (
-                            <section
-                                className="tech__con"
-                                key={technology.name}
-                            >
-                                <p className="intro__text tech__intro__text">
-                                    <span className="index">03</span>Space
-                                    launch 101
-                                </p>
+        <main className="tech__main__con">
+            {technologyData.map(
+                (technology) =>
+                    techName === technology.name && (
+                        <section className="tech__con" key={technology.name}>
+                            <p className="intro__text tech__intro__text">
+                                <span className="index">03</span>Space launch
+                                101
+                            </p>
 
-                                <section className="tech__dets__con">
-                                    <div className="tech__img__con">
-                                        <img
-                                            src={`src/${technology.images.landscape.mobile}`}
-                                            className="mobile__img"
-                                            alt={`Image of a ${technology.name}`}
-                                        />
-                                        <img
-                                            src={`src/${technology.images.landscape.tablet}`}
-                                            className="tablet__img"
-                                            alt={`Image of a ${technology.name}`}
-                                        />
-                                        <img
-                                            src={`src${technology.images.portrait}`}
-                                            className="desktop__img"
-                                            alt={`Image of a ${technology.name}`}
-                                        />
-                                    </div>
-                                    <section className="tech__dets">
-                                        <nav className="tech__nav__con">
-                                            {technologyData.map(
-                                                (techNav, index) => (
-                                                    <div
-                                                        onClick={() =>
-                                                            changeTech(
-                                                                techNav.name
-                                                            )
-                                                        }
-                                                        key={techNav.name}
-                                                        className={`tech__nav__item ${
-                                                            techName ===
-                                                            techNav.name
-                                                                ? "tech__nav__item--active"
-                                                                : ""
-                                                        }`}
-                                                    >
-                                                        {index + 1}
-                                                    </div>
-                                                )
-                                            )}
-                                        </nav>
+                            <section className="tech__dets__con">
+                                <div className="tech__img__con">
+                                    <img
+                                        src={`src/${technology.images.landscape.mobile}`}
+                                        className="mobile__img"
+                                        alt={`Image of a ${technology.name}`}
+                                    />
+                                    <img
+                                        src={`src/${technology.images.landscape.tablet}`}
+                                        className="tablet__img"
+                                        alt={`Image of a ${technology.name}`}
+                                    />
+                                    <img
+                                        src={`src${technology.images.portrait}`}
+                                        className="desktop__img"
+                                        alt={`Image of a ${technology.name}`}
+                                    />
+                                </div>
+                                <section className="tech__dets">
+                                    <nav className="tech__nav__con">
+                                        {technologyData.map(
+                                            (techNav, index) => (
+                                                <div
+                                                    onClick={() =>
+                                                        changeTech(techNav.name)
+                                                    }
+                                                    key={techNav.name}
+                                                    className={`tech__nav__item ${
+                                                        techName ===
+                                                        techNav.name
+                                                            ? "tech__nav__item--active"
+                                                            : ""
+                                                    }`}
+                                                >
+                                                    {index + 1}
+                                                </div>
+                                            )
+                                        )}
+                                    </nav>
 
-                                        <section className="tech__details">
-                                            <h2 className="tech__cat">
-                                                The terminology...
-                                            </h2>
-                                            <h1 className="tech__name">
-                                                {technology.name}
-                                            </h1>
-                                            <p className="tech__desc">
-                                                {technology.description}
-                                            </p>
-                                        </section>
+                                    <section className="tech__details">
+                                        <h2 className="tech__cat">
+                                            The terminology...
+                                        </h2>
+                                        <h1 className="tech__name">
+                                            {technology.name}
+                                        </h1>
+                                        <p className="tech__desc">
+                                            {technology.description}
+                                        </p>
                                     </section>
                                 </section>
                             </section>
-                        )
-                )}
-            </main>
-        </>
+                        </section>
+                    )
+            )}
+        </main>
     );
 }
 
